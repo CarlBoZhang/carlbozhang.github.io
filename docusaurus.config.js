520c538ffa893e910793cc34fefc8ea45574ebb0
Carl <carl.hku@gmail.com>
2022-12-27 12:00:56 +0800

    initial

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://carl-cyberlab.github.io',
+  url: 'https://carlbozhang.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'carlbozhang', // Usually your GitHub org/user name.
-  projectName: 'carl-cyberlab.github.io', // Usually your repo name.
+  projectName: 'carlbozhang.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
